8b9d4072f9598fa1a5d82960dac007b4deafcf7e
richu shaji abraham <richursa@gmail.com>
2018-10-08 08:10:10 +0530

    made changes and refactored code

--- cpuOne.cpp
+++ cpuOne.cpp
@@ -1,11 +1,35 @@
-#include<iostream>
-#include<stdio.h>
 #include<fstream>
-#include<time.h>
-using namespace std;
+#include<stdio.h>
+#include "random.h"
+long long int  read_file_to_memmory(FILE *pInfile , int *pPointer)
+{
+    if(pInfile != NULL)
+    {
+        
+        int mIndex =0;
+        int mSize = fread(pPointer+mIndex,1,sizeof(int),pInfile);
+        long long int mFileSize=0;
+        while(mSize!= 0)
+        {
+            mFileSize = mFileSize +mSize;
+            mIndex++;
+            mSize = fread(pPointer+mIndex,1,mSize,pInfile);
+        }
+        return mFileSize;
+    }
+    return 0;
+}
+long long int write_file_from_memmory(FILE *pOutFile , int *pPointer,long long int pFileSize)
+{
+    if(pOutFile!=NULL)
+    {
+        pFileSize = fwrite(pPointer,1,pFileSize,pOutFile);
+        return pFileSize;
+    }
+    return 0;
+}
 int main()
 {
-    
     FILE *infile;
     infile = fopen("file.txt","rb");
     FILE *outfile;
@@ -14,9 +38,7 @@ int main()
     outfile = fopen("enc.txt","wb");
     int *buffer  = new int;
     int *xorVal = new int;
-    time_t t;
     int size;
-    srand((unsigned) time(&t));
     if(infile!= NULL)
     {
      size =fread(buffer,1,sizeof(int),infile);
